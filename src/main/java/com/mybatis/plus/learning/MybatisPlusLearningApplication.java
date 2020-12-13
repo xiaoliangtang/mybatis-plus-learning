d48fa8d4281c97a706e3a540b964b8d360238aff
xiaoliangtang <xiaoliangtang@yaochufa.com>
2020-12-13 09:02:14 +0800

    [add]add basic files.

---
+++ src/main/java/com/mybatis/plus/learning/MybatisPlusLearningApplication.java
@@ -0,0 +1,13 @@
+package com.mybatis.plus.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPlusLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybatis/plus/learning/MybatisPlusLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybatis.plus.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
